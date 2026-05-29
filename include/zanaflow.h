0a4f5736a9345cbff43e68f7367fef471bda0874
Mobin Abdi <Mobinabdi298@gmail.com>
2026-05-29 15:01:44 +0330

    feat: add uberella file zanaflow.h

---
+++ include/zanaflow.h
@@ -0,0 +1,27 @@
+#ifndef ZANAFLOW_H
+#define ZANAFLOW_H
+
+// Core & Utils
+#include <zanaflow/core/parameter.h>
+#include <zanaflow/utils/utils.h>
+
+// Tensor Operations
+#include <zanaflow/tensor/tensor.h>
+#include <zanaflow/ops/ops.h>
+
+// Neural Network Layers
+#include <zanaflow/nn/dense.h>
+
+// Loss Functions
+#include <zanaflow/loss/loss.h>
+
+// Optimizers
+#include <zanaflow/optim/sgd.h>
+
+// Activation functions
+#include <zanaflow/ops/activations.h>
+
+// matrix multiply
+#include <zanaflow/ops/linalg.h>
+
+#endif

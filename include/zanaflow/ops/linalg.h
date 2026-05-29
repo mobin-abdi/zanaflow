ca2ae645dedbfc441e9cd4049571ae6723057ccf
Mobin Abdi <Mobinabdi298@gmail.com>
2026-05-29 14:30:42 +0330

    feat: add the matrix multiply

---
+++ include/zanaflow/ops/linalg.h
@@ -0,0 +1,8 @@
+#ifndef LINALG_H
+#define LINALG_H
+
+#include <zanaflow/tensor/tensor.h>
+
+Tensor *tensor_matmul_2d(const Tensor *a, const Tensor *b);
+
+#endif
---
+++ src/ops/linalg.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <zanaflow/ops/linalg.h>\
+
+Tensor *tensor_matmul_2d(const Tensor *a, const Tensor *b)
+{
+    if (a == NULL || b == NULL)
+    {
+        return NULL;
+    }
+
+    if (a -> ndim != 2 || b -> ndim != 2)
+    {
+        return NULL;
+    }
+
+    int m = a->shape[0];
+    int n = a->shape[1];
+    int n2 = b->shape[0];
+    int p = b->shape[1];
+
+    if (n != n2)
+    {
+        return NULL;
+    }
+
+    int out_shape[2] = { m, p };
+    Tensor *out = tensor_create(out_shape, 2);
+
+    if (out == NULL)
+    {
+        return NULL;
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            float sum = 0.0f;
+            for (int k = 0; k < n; k++) {
+                float av = a->data[i * n + k];
+                float bv = b->data[k * p + j];
+                sum += av * bv;
+            }
+            out->data[i * p + j] = sum;
+        }
+    }
+
+    return out;
+}
